df92b0694cfddefa58db0d7d577d163461d123a7
bakuppus <31225958+bakuppus@users.noreply.github.com>
2019-05-19 03:38:03 +0530

    Update dockerclean.sh

--- dockerclean.sh
+++ dockerclean.sh
@@ -1,7 +1,16 @@
-dockps=$(docker ps | grep -i dorapos- | awk '{print $1}' | tail -n1);
-if [ $dockps ];
-then
-/usr/bin/docker stop $dockps
-else
-echo "no contianer running"
+#!/bin/bash
+CONTAINER_NAME1="jhipster"
+OLD1="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME1")"
+if [ -n "$OLD1" ]; then
+  docker stop $OLD1 && docker rm $OLD1
+fi
+CONTAINER_NAME2="elasticsearch"
+OLD2="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME2")"
+if [ -n "$OLD2" ]; then
+  docker stop $OLD2 && docker rm $OLD2
+fi
+CONTAINER_NAME3="postgresql"
+OLD3="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME3")"
+if [ -n "$OLD3" ]; then
+  docker stop $OLD3 && docker rm $OLD3
 fi
