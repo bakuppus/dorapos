70de663b950ea11269641bb3689b09f61f9be171
bakuppus <31225958+bakuppus@users.noreply.github.com>
2019-05-22 06:07:59 +0530

    Update dockerclean.sh

--- dockerclean.sh
+++ dockerclean.sh
@@ -4,13 +4,13 @@ OLD1="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME1")"
 if [ -n "$OLD1" ]; then
   docker stop $OLD1 && docker rm $OLD1
 fi
-CONTAINER_NAME2="elasticsearch"
-OLD2="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME2")"
-if [ -n "$OLD2" ]; then
-  docker stop $OLD2 && docker rm $OLD2
-fi
-CONTAINER_NAME3="postgresql"
-OLD3="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME3")"
-if [ -n "$OLD3" ]; then
-  docker stop $OLD3 && docker rm $OLD3
-fi
+#CONTAINER_NAME2="elasticsearch"
+#OLD2="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME2")"
+#if [ -n "$OLD2" ]; then
+#  docker stop $OLD2 && docker rm $OLD2
+#fi
+#CONTAINER_NAME3="postgresql"
+#OLD3="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME3")"
+#if [ -n "$OLD3" ]; then
+#  docker stop $OLD3 && docker rm $OLD3
+#fi
